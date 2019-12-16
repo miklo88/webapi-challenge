305253360366673c040499419311a212be270421
Carlos <miklo.concepcion@gmail.com>
2019-12-15 23:44:04 -0600

    crud operation finished for projects => projectsRouter. Moving back to actions => actionsRouter CRUD operations

--- data/routers/projectsRouter.js
+++ data/routers/projectsRouter.js
@@ -3,7 +3,9 @@ const express = require("express");
 const router = express.Router();
 
 const Projects = require("../helpers/projectModel");
+
 // PROJECTS CRUD
+// GET
 router.get("/", (req, res) => {
   Projects.get()
     .then(projects => {
@@ -15,10 +17,10 @@ router.get("/", (req, res) => {
         message: "Error 500"
       });
     });
-}); // READ / GET
+}); // READ / GET 0
 
 router.get("/:id", (req, res) => {
-  Projects.getProjectActions(req.params.id)
+  Projects.get(req.params.id)
     .then(project => {
       res.status(200).json(project);
     })
@@ -27,22 +29,42 @@ router.get("/:id", (req, res) => {
         error: "Error req projects - GET server"
       });
     });
-});
+}); // READ / GET 1
 
+router.get("/:id/actions", (req, res) => {
+  Projects.getProjectActions(req.params.id)
+    .then(projectActions => {
+      res.status(201).json(projectActions);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "Error req projects - GET server"
+      });
+    });
+}); // READ / GET 2
+
+// POST
 router.post("/", (req, res) => {
-  res.status(201).json({
-    url: "/projects",
-    message: "post a project",
-    operation: "POST"
-  });
+  Projects.insert(req.body)
+    .then(newResource => {
+      res.status(200).json(newResource);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "Error req projects - POSTS server"
+      });
+    });
 }); // CREATE ON THE BODY OBJECT
-
+// PUT
 router.put("/", (req, res) => {
-  res.status(200).json({
-    url: "/projects",
-    message: "update a project",
-    operation: "PUT"
-  });
+  Projects.update(req.params.id, req.body)
+    .then(updatedResource => {
+      res.status(200).json(updatedResource);
+    })
+    .catch(err => {
+      console.log("Error req projects - PUT server");
+      res.status(500).json({ message: "Error on PUT server" });
+    });
 }); // UPDATE ON THE BODY OBJECT
 
 router.delete("/", (req, res) => {
@@ -51,14 +73,23 @@ router.delete("/", (req, res) => {
     message: "deleted",
     operation: "DELETE"
   });
-}); // DELETE
+}); // DELETE TEST ON 204
 
 router.delete("/:id", (req, res) => {
   const id = req.params.id;
   console.log(req.params); // to see data implemented when console.log(). ex id = user
-  res.status(204).json({
-    url: "/projects"
-  });
+  Projects.remove(req.params.id)
+    .then(projectDeleted => {
+      res.status(200).json({
+        message: `You erased ${projectDeleted}`
+      });
+    })
+    .catch(err => {
+      console.log("Error req projects - DELETE server");
+      res
+        .status(500)
+        .json({ error: "Failed to delete req project - DELETE server" });
+    });
 }); // DELETE
 
 module.exports = router;
