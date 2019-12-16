f01efea0c1b5df0ec840c1ffaf1b93930e0ded01
Carlos <miklo.concepcion@gmail.com>
2019-12-16 01:15:08 -0600

    deploying to heroku. adding .env file. cleaning up the code.

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ const server = require("./server.js");
 const port = 5000;
 
 server.listen(port, () => {
-  console.log(`server listening on port ${port}`);
+  console.log(`FBI listening on https://localhost:${port}`);
 });
